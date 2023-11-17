3f7f563985e906324cac1b09c50ba3f9e25bc9f0
chaithanya <varma@gmail.com>
2023-11-17 14:48:47 +0530

    Added the menu based approach

--- src/Main.java
+++ src/Main.java
@@ -18,35 +18,26 @@ public class Main{
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter 1st number");
-        double var1=sc.nextDouble();
-        System.out.println("enter 2nd number");
-        double var2=sc.nextDouble();
-        System.out.println("Enter your choice\n 1: Addition \n 2: Subtraction\n 3: Multiplication\n 4: Division\n 5: Modulus");
-        int choice=sc.nextInt();
-        double var3;
-        switch (choice){
-            case 1:
-                System.out.println("sum of two numbers is :" +addition(var1,var2));
-                break;
-            case 2:
-                System.out.println("sub of two numbers is :" +subtraction(var1,var2));
-                break;
-            case 3:
-                System.out.println("mul of two numbers is :" +multiplication(var1,var2));
-                break;
-            case 4:
-                System.out.println("div of two numbers is :" +division(var1,var2));
-                break;
-            case 5:
-                System.out.println("mod of two numbers is :" +modulus(var1,var2));
-                break;
-            default:
-                System.out.println("Invalid option");
-                break;
+        int option=0;
+        do {
+            System.out.println("enter 1st number");
+            double var1 = sc.nextDouble();
+            System.out.println("enter 2nd number");
+            double var2 = sc.nextDouble();
+            System.out.println("Enter your choice\n 1: Addition \n 2: Subtraction\n 3: Multiplication\n 4: Division\n 5: Modulus");
+            int choice = sc.nextInt();
+            double var3;
+            switch (choice) {
+                case 1 -> System.out.println("sum of two numbers is :" + addition(var1, var2));
+                case 2 -> System.out.println("sub of two numbers is :" + subtraction(var1, var2));
+                case 3 -> System.out.println("mul of two numbers is :" + multiplication(var1, var2));
+                case 4 -> System.out.println("div of two numbers is :" + division(var1, var2));
+                case 5 -> System.out.println("mod of two numbers is :" + modulus(var1, var2));
+                default -> System.out.println("Invalid option");
+                }
+            System.out.println("To end the program press 1");
+            option = sc.nextInt();
+            } while(option!=1);
 
-
-
-        }
     }
 }
